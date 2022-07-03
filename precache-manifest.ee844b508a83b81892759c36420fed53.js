d9c51d63fcdf4a39c2aeae59b1a0282107f3a443
faraji-fuji <farajifuji@gmail.com>
2022-07-03 15:58:45 +0300

    Updates

---
+++ precache-manifest.ee844b508a83b81892759c36420fed53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3651abf2d369b79c611b",
+    "url": "/robofriends/static/css/main.958b6ff2.chunk.css"
+  },
+  {
+    "revision": "3651abf2d369b79c611b",
+    "url": "/robofriends/static/js/main.3651abf2.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "7d557a3512aa4cb4e113",
+    "url": "/robofriends/static/css/2.27cc2526.chunk.css"
+  },
+  {
+    "revision": "7d557a3512aa4cb4e113",
+    "url": "/robofriends/static/js/2.7d557a35.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "08c4bc3cbd5f72889ec33820e09aaeaf",
+    "url": "/robofriends/index.html"
+  }
+];
