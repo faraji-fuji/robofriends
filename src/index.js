33806cd48d9c05216c6f2b4ecc02732eee137332
faraji-fuji <farajifuji@gmail.com>
2022-07-03 13:14:24 +0300

    search functionality

--- src/index.js
+++ src/index.js
@@ -3,20 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import "tachyons";
-import Card from "./Card";
-import {robots} from "./robots";
+import App from "./App.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   //   <Card/>
   // </React.StrictMode>
-
-  <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-  </div>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
