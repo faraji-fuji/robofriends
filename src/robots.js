bdf289026791c8dac02942ff4898bc3be3ee7e66
faraji-fuji <farajifuji@gmail.com>
2022-07-03 11:08:31 +0300

    create card component

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'faraji shikanda',
+        username: 'faraji',
+        email: 'fara@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'mike tyson',
+        username: 'tyson',
+        email: 'mike.tyson@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'imbungu mike',
+        username: 'micah',
+        email: 'mike.imbungu@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'philip dadi',
+        username: 'dadiyo',
+        email: 'dadiyo@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'oti otieno',
+        username: 'bakayoko',
+        email: 'oti.baks@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'joy favor',
+        username: 'fav',
+        email: 'fav.joy@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'benson mitchel',
+        username: 'ben',
+        email: 'ben.mitch@gmail.com'
+    },
+]
